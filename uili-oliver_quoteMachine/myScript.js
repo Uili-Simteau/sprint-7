1ebc3cf2d6251512c136c540e34593f57078cfab
Oliver Stigley <oliverstigley@gmail.com>
2016-04-06 12:26:32 +1200

    this commit will push jquery mod

--- uili-oliver_quoteMachine/myScript.js
+++ uili-oliver_quoteMachine/myScript.js
@@ -1,7 +1,42 @@
 
 	document.getElementById('getQuote').onclick = function() {
+		var url = "https://andruxnet-random-famous-quotes.p.mashape.com/?cat=movies";
+		$.ajax(
+		{
+			url: url,
+			type: 'POST',
+			dataType: 'JSON',
+			async: false,	
+			headers: {
+				"X-Mashape-Key": "YYZBL44AScmshjOI6TkpIvwUK4fpp1uQLUcjsnM16uN9HQH3ti",
+				"Content-Type": "application/x-www-form-urlencoded",
+				Accept: "application/json"
+			},
+
+			success: function(data) {
+				var quoteDiv = document.querySelector('#quote');
+				quoteDiv.innerHTML = "";
+				var quoteContent = document.createElement('p');
+				quoteContent.innerHTML = data.quote;
+				console.log(quoteContent)
+				quoteDiv.appendChild(quoteContent);
+				
+				var quoteAuthor = document.createElement('p');
+				quoteAuthor.innerHTML = data.author;
+				console.log(quoteAuthor);
+				quoteDiv.appendChild(quoteAuthor);
+			}
+
+		}
+
+
+		)
+
+
+
+
+		/*
 		var newQuote = new XMLHttpRequest();
-		var url = "https://andruxnet-random-famous-quotes.p.mashape.com/";
 		newQuote.open('POST', url, false);
 		newQuote.setRequestHeader("X-Mashape-Key", "YYZBL44AScmshjOI6TkpIvwUK4fpp1uQLUcjsnM16uN9HQH3ti");
 		newQuote.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
@@ -22,6 +57,7 @@
 		quoteAuthor.innerHTML = data.author;
 		console.log(quoteAuthor);
 		quoteDiv.appendChild(quoteAuthor);
+		*/
 
 	};
 
